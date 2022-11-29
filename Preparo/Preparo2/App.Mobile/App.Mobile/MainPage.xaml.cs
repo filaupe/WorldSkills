4d9ece2d7ec92260c01813f931f4f9b7b1f1c7c9
MulinhaGPlays <83516034+MulinhaGPlays@users.noreply.github.com>
2022-11-29 06:44:57 -0300

    preparo 2

---
+++ Preparo/Preparo2/App.Mobile/App.Mobile/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App.Mobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Preparo/Preparo2/App/Models/teste1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Web;
+
+namespace App.Models
+{
+    public class teste1
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public static implicit operator teste1(string Json)
+        {
+            DataContractJsonSerializer jsonSer = new DataContractJsonSerializer(typeof(teste1));
+            MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(Json));
+            teste1 objteste1 = (teste1)jsonSer.ReadObject(stream);
+
+            return new teste1()
+            {
+                Id = objteste1.Id,
+                Name = objteste1.Name,
+            };
+        }
+    }
+}
