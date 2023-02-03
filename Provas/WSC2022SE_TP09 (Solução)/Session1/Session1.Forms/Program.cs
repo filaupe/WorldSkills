fb867c8e49158637f435ebfa4dd00b879c700886
MulinhaGPlays <83516034+MulinhaGPlays@users.noreply.github.com>
2023-02-03 18:00:14 -0200

    Documentação traduzida

---
+++ Provas/WSC2022SE_TP09 (Solução)/Session1/Session1.Forms/Program.cs
@@ -0,0 +1,19 @@
+using Session1.Forms.Telas;
+
+namespace Session1.Forms
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Principal());
+        }
+    }
+}
---
+++ Provas/WSC2022SE_TP09 (Solução)/Session1/Session1.Forms/Telas/Principal.cs
@@ -0,0 +1,17 @@
+﻿using Session1.Infra.Models;
+
+namespace Session1.Forms.Telas;
+
+public partial class Principal : Form
+{
+    public Principal()
+    {
+        InitializeComponent();
+    }
+
+    private void Principal_Load(object sender, EventArgs e)
+    {
+        Session1Context context = new();
+        this.dataGridView.DataSource = context.ItemAmenities.ToList();
+    }
+}
